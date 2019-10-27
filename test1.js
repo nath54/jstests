e6dec9450198d23eae3c4f8a88bda35f162a5382
nath54 <cerisara.nathan@gmail.com>
2019-10-27 14:39:06 +0100

    a

--- test1.js
+++ test1.js
@@ -1,22 +1,20 @@
 canvas=document.getElementById("canvas");
 ctx=canvas.getContext("2d");
 
+
 var encourAnim=true;
-var miny=-10;
-var maxy=10;
-var avy=1;
 
 var dt=new Date();
 var dan=dt.getTime();
 var tpan=0;
 
-var tex=1000;
-var tey=1000;
+var tex=parseInt(canvas.width);
+var tey=parseInt(canvas.height);
 
 var couleurs=["rgb(100,200,100)","rgb(200,100,100)","rgb(100,100,200)","rgb(200,200,100)","rgb(100,200,200)","rgb(0,100,200)","rgb(200,0,100)","rgb(200,100,0)","rgb(100,100,100)","rgb(200,0,250)","rgb(250,100,0)","rgb(0,250,0)","rgb(10,0,250)"]
 
 function f(num,x){
-    var y=parseInt(document.getElementById("Y").value);
+    var y=parseFloat(document.getElementById("Y").value);
     var ff=null;
     try{
         eval("var ff="+document.getElementById("f"+num).value+";");
@@ -29,8 +27,10 @@ function f(num,x){
 }
 
 function anim(){
-    var y=parseInt(document.getElementById("Y").innerHTML);
-    
+    var y=parseFloat(document.getElementById("Y").innerHTML);
+    var miny=parseFloat(document.getElementById("miny").value);
+    var maxy=parseFloat(document.getElementById("maxy").value);
+    var avy=parseFloat(document.getElementById("avy").value);
     y+=avy;
     if(y>=maxy){
         y=maxy;
@@ -40,6 +40,7 @@ function anim(){
         y=miny;
         avy=-avy;
     }
+    document.getElementById("avy").value=avy;
     document.getElementById("Y").value=y;
     document.getElementById("Y").innerHTML=y;
 }
@@ -57,23 +58,25 @@ function aff(){
     ctx.lineTo(parseInt(tex/2),tey);
     ctx.stroke()
     for(w=1;w<10;w++){
-        ctx.beginPath();
-        ctx.strokeStyle=couleurs[w];
-        ctx.moveTo(-tex/2,tey/2-f(w,-tex/2));
-        for(x=-tex/2;x<tex/2;x++){
-            try{
-                val=f(w,x);
-                ctx.lineTo(tex/2-x,tey/2-val);
+        if( document.getElementById("f"+w).value != ""){
+            ctx.beginPath();
+            ctx.strokeStyle=couleurs[w];
+            x=-tex/2
+            val=f(w,x);
+            ctx.lineTo(tex/2-x,tey/2-val);
+            for(x=-tex/2;x<tex/2;x++){
+                try{
+                    val=f(w,x);
+                    ctx.lineTo(tex/2-x,tey/2-val);
+                }
+                catch(error){
+                
+                }
             }
-            catch(error){
-            
-            }
+            ctx.stroke();
         }
-        ctx.stroke();
     }
 }
-
-
 
 
 
